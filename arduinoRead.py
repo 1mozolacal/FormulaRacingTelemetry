b1ced1ab18348a8f76f3ecbf53db7d2ca10583ce
Calvin Mozola <calvin@stuntfactory.com>
2018-11-17 14:11:21 -0500

    Modified reading from config file
    Made it more salable and modular

--- arduinoRead.py
+++ arduinoRead.py
@@ -10,14 +10,32 @@ import datetime
 def readConfigFile():
     configSettings = {}#return this dict
     configFile = open("config.txt", "r")#opens config file
-    namesLine = configFile.readline().split(";")  # gets array
-    dataNames = {}  # Kinda of like an enum for the different values
-    for i in range(0, len(namesLine), 2):
-        dataNames[int(namesLine[i + 1])] = namesLine[i]#the number is the key string is the value
-    dataFocus = configFile.readline()
-    dataFocusNum = int(dataFocus[6:len(dataFocus)])#removes the "graph=" part
-    configSettings["dataNum"] = dataFocusNum
-    configSettings["dataName"] = dataNames[dataFocusNum]
+    namesLineArray = configFile.readline().split(";")  # gets array
+    namesLineDict = {}  # Kinda of like an enum for the different values
+    for i in range(0, len(namesLineArray), 2):
+        namesLineDict[int(namesLineArray[i + 1])] = namesLineArray[i]  # the number is the key string is the value
+
+    dataSelected = configFile.readline()
+    dataSelected = dataSelected[6:len(dataSelected)]#removes the "graph=" part
+    dataSelectedArray = dataSelected.replace(" ","").split(',')
+    for i in range(0,len(dataSelectedArray)):#converts the array of strings to integers
+        dataSelectedArray[i] = int(dataSelectedArray[i])
+
+    #the number of different things that can customize a dataset(in the future there will be ranges and so on)
+    differentDataFeatures = 2
+    #0 = (int) the number
+    #1 = (String) the name
+    dataConfigArray =  [[None for x in range(differentDataFeatures)] for y in range(len(dataSelectedArray))] # when calling[Which type of data][the feature]
+    # note make a default one to replace if a None value is found
+
+    for i in range(0,len(dataSelectedArray)):
+        dataConfigArray[i][0] = dataSelectedArray[i]#the number
+        dataConfigArray[i][1] = namesLineDict[dataSelectedArray[i]]#the name
+
+
+
+    configSettings["dataConfig"] = dataConfigArray
+
     return configSettings#returns the varible inputed by a
 
 systemVar = readConfigFile()#a dict of user inputed varibles
